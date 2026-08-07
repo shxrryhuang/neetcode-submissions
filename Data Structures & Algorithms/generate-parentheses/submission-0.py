0048f49e67205f5b89fa6e8e280ed748594192f6
shxrryhuang <shxrryhuang@gmail.com>
2026-08-07 17:11:44 -0400

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        res = []
+        def backtrack(path,opn,close):
+            if len(path)==2*n:
+                res.append("".join(path))
+                return
+
+            if opn<n:
+                path.append("(")
+                backtrack(path,opn+1,close)
+                path.pop()
+            if close<opn:
+                path.append(")")
+                backtrack(path,opn,close+1)
+                path.pop()
+        backtrack([],0,0)
+        return res
