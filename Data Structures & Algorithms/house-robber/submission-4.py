8bb53b6d9e39516302cd49ec9a379c7a2b9ae08b
shxrryhuang <shxrryhuang@gmail.com>
2026-08-16 00:23:57 -0400

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.py
@@ -0,0 +1,18 @@
+from functools import cache
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+    
+        memo = [-1]*len(nums) #initialize memo
+
+        def dp(i):
+            if i>=len(nums):
+                return 0 
+            if memo[i]!= -1: #if computed already, return memo
+                return memo[i]
+            memo[i]=max(dp(i+1),nums[i]+dp(i+2)) #else compute it
+            return memo[i]
+            
+        return max(dp(0),dp(1))
+
+        
